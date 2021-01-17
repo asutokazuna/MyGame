55bde4e6b44a2bf6be14de730d8651a54c333c86
asuto <asutokazuna1110@gmail.com>
2021-01-17 22:18:58 +0900

    カメラの動きを軸方向に割合で動けるように変更

--- sampleFBX/CTPCamera.cpp
+++ sampleFBX/CTPCamera.cpp
@@ -38,6 +38,8 @@ HRESULT CTPCamera::Init()
 		transform->position +=  m_transform->position;
 		m_vLook =  m_transform->position + g_vLook;
 	}
+	Vector3 dir = transform->position - m_vLook;
+	transform->quaternion = MyMath::LookAt(dir);
 	UpdateMatrix();
 	return E_NOTIMPL;
 }
@@ -48,6 +50,7 @@ void CTPCamera::Uninit()
 }
 static XMFLOAT4X4 mmtxWorld;
 static Transform mtrs;
+Vector3 pos;
 /**
  * @brief 更新処理
  * @return なし
@@ -63,18 +66,16 @@ void CTPCamera::LateUpdate()
 	mtrs.localPosition = Vector3(g_vEye.x, g_vEye.y, g_vEye.z);
 	//m_vLook = m_transform->position + g_vLook;
 
-	transform->quaternion = mtrs.quaternion;
 
 	mmtxWorld =
 		MyMath::StoreXMFloat4x4(mtrs);
 	mtrs.localPosition = Vector3(g_vLook.x, g_vLook.y, g_vLook.z);
 	XMFLOAT4X4 mtxWorld = MyMath::StoreXMFloat4x4(mtrs);
 
-	Vector3 pos = Vector3(mmtxWorld._41, mmtxWorld._42, mmtxWorld._43);
+	pos = Vector3(mmtxWorld._41, mmtxWorld._42, mmtxWorld._43);
 	Vector3 look = Vector3(mtxWorld._41, mtxWorld._42, mtxWorld._43);
 	mtrs.localPosition = Vector3(g_vUp.x, g_vUp.y, g_vUp.z);
 	mtxWorld = MyMath::StoreXMFloat4x4(mtrs);
-	m_vUp = MyMath::PosxQuaternion(g_vUp, transform->quaternion);
 	//m_vUp = Vector3(mtxWorld._41, mtxWorld._42, mtxWorld._43);
 
 //	transform->position = pos;
@@ -86,9 +87,43 @@ void CTPCamera::LateUpdate()
 	//XMStoreFloat3(&m_vLook, XMVector3TransformCoord (XMLoadFloat3(&g_vLook), world));
 	//XMStoreFloat3(&m_vUp  , XMVector3TransformNormal(XMLoadFloat3(&g_vUp), world));
 
-	const float ratio = 0.97f;
-	transform->position = transform->position * ratio + pos * (1 - ratio);
-	m_vLook = m_vLook * ratio + look * (1 - ratio);
+	const float ratioX = 0.93f;
+	const float ratioY = 0.5f;
+	const float ratioZ = 0.7f;
+	Vector3 add = Vector3();
+	/*
+	// x,y,z軸
+	add.x = transform->position.x * ratioX + pos.x * (1 - ratioX);
+	add.y = transform->position.y * ratioY + pos.y * (1 - ratioY);
+	add.z = transform->position.z * ratioZ + pos.z * (1 - ratioZ);
+	transform->position = add;
+	*/
+
+	//add += transform->position * MyMath::Abs(transform->GetRight())	*ratioX + MyMath::Abs(transform->GetRight())	* pos * (1 - ratioX);
+	//add += transform->position * MyMath::Abs(transform->GetUp())		*ratioY + MyMath::Abs(transform->GetUp())		* pos * (1 - ratioY);
+	//add += transform->position * MyMath::Abs(transform->GetForward())	*ratioZ + MyMath::Abs(transform->GetForward())	* pos * (1 - ratioZ);
+	//transform->position = add;
+
+	add += (transform->GetRight())	* MyMath::Dot(transform->GetRight(), transform->position)	*ratioX + (transform->GetRight())	* MyMath::Dot(transform->GetRight(), pos)	* (1 - ratioX);
+	add += (transform->GetUp())		* MyMath::Dot(transform->GetUp(), transform->position)		*ratioY + (transform->GetUp())		* MyMath::Dot(transform->GetUp(), pos)		* (1 - ratioY);
+	add += (transform->GetForward())* MyMath::Dot(transform->GetForward(), transform->position) *ratioZ + (transform->GetForward())	* MyMath::Dot(transform->GetForward(), pos) * (1 - ratioZ);
+	transform->position = add;
+
+	Vector3 lookadd = Vector3();
+	lookadd += (transform->GetRight())	* MyMath::Dot(transform->GetRight(), m_vLook)	*ratioX + (transform->GetRight())	* MyMath::Dot(transform->GetRight(), look)	* (1 - ratioX);
+	lookadd += (transform->GetUp())		* MyMath::Dot(transform->GetUp(), m_vLook)		*ratioY + (transform->GetUp())		* MyMath::Dot(transform->GetUp(), look)		* (1 - ratioY);
+	lookadd += (transform->GetForward())* MyMath::Dot(transform->GetForward(), m_vLook) *ratioZ + (transform->GetForward())	* MyMath::Dot(transform->GetForward(), look) * (1 - ratioZ);
+	m_vLook = lookadd;
+
+
+	//add = pos - transform->position;
+	//Vector3 move = Vector3();
+	//move += transform->GetRight() * pos.x;
+	//transform->position += add;
+
+
+	//transform->position = transform->position * ratioX + pos * (1 - ratioX);
+	//m_vLook = m_vLook * ratioX + look * (1 - ratioX);
 	// 始点、注視点、上方ベクトルを近づける
 	//m_vNowEye.x = m_vNowEye.x   * ratio + m_vEye.x  * (1 - ratio);
 	//m_vNowEye.y = m_vNowEye.y   * ratio + m_vEye.y  * (1 - ratio);
@@ -100,6 +135,9 @@ void CTPCamera::LateUpdate()
 	//m_vNowUp.y = m_vNowUp.y     * ratio + m_vUp.y   * (1 - ratio);
 	//m_vNowUp.z = m_vNowUp.z     * ratio + m_vUp.z   * (1 - ratio);
 
+	Vector3 dir = m_vLook - transform->position;
+	transform->quaternion = MyMath::LookAt(dir);
+	m_vUp = MyMath::PosxQuaternion(g_vUp, transform->quaternion);
 	// 行列更新
 	CCamera::UpdateMatrix();
 }
@@ -108,10 +146,12 @@ void CTPCamera::Draw()
 {
 #ifdef _DEBUG
 	if (ImGui::TreeNode("TPCamera")) {
-		Vector3 pos = transform->position;
 		ImGui::SliderFloat("pos x", &pos.x, -1000.0f, 500.0f);
 		ImGui::SliderFloat("pos y", &pos.y, -1000.0f, 500.0f);
 		ImGui::SliderFloat("pos z", &pos.z, -1000.0f, 500.0f);
+		ImGui::SliderFloat("eyepos x", &transform->position.x, -1000.0f, 500.0f);
+		ImGui::SliderFloat("eyepos y", &transform->position.y, -1000.0f, 500.0f);
+		ImGui::SliderFloat("eyepos z", &transform->position.z, -1000.0f, 500.0f);
 		ImGui::SliderFloat("Look x", &m_vLook.x, -1000.0f, 500.0f);
 		ImGui::SliderFloat("Look y", &m_vLook.y, -1000.0f, 500.0f);
 		ImGui::SliderFloat("Look z", &m_vLook.z, -1000.0f, 500.0f);
@@ -127,10 +167,10 @@ void CTPCamera::Draw()
 		ImGui::SliderFloat("GetUp x", &te.x, -1.0f, 1.0f);
 		ImGui::SliderFloat("GetUp y", &te.y, -1.0f, 1.0f);
 		ImGui::SliderFloat("GetUp z", &te.z, -1.0f, 1.0f);
-		//Vector3 tete = mtrs.GetUp();
-		//ImGui::SliderFloat("GetUp x", &tete.x, -1.0f, 1.0f);
-		//ImGui::SliderFloat("GetUp y", &tete.y, -1.0f, 1.0f);
-		//ImGui::SliderFloat("GetUp z", &tete.z, -1.0f, 1.0f);
+		Vector3 tete = transform->GetForward();
+		ImGui::SliderFloat("GetForward x", &tete.x, -1.0f, 1.0f);
+		ImGui::SliderFloat("GetForward y", &tete.y, -1.0f, 1.0f);
+		ImGui::SliderFloat("GetForward z", &tete.z, -1.0f, 1.0f);
 		//Vector3 qut = transform->quaternion.x;
 		ImGui::SliderFloat("qut x", &transform->quaternion.x, -1.0f, 1.0f);
 		ImGui::SliderFloat("qut y", &transform->quaternion.y, -1.0f, 1.0f);
